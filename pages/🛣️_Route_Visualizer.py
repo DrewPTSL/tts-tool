33d0200032596ff110770c4b54408ddfadbe2d75
DrewPTSL <dcobean@ptsl.com>
2026-09-23 13:45:13 -0400

    adding carto api key

--- pages/🛣️_Route_Visualizer.py
+++ pages/🛣️_Route_Visualizer.py
@@ -66,12 +66,7 @@ def get_route(start_coords, end_coords):
 def create_map(start_coords, end_coords, route_data):
     center_lat = (start_coords[0] + end_coords[0]) / 2
     center_lon = (start_coords[1] + end_coords[1]) / 2
-    m = folium.Map(
-        location=[center_lat, center_lon],
-        zoom_start=12,
-        tiles="CartoDB positron",
-        width='100%'
-    )
+    m = folium.Map(location=[center_lat, center_lon], zoom_start=12, tiles=f"https://{{s}}.basemaps.cartocdn.com/light_all/{{z}}/{{x}}/{{y}}.png?api_key={st.secrets['CARTO_API_KEY']}", attr="© OpenStreetMap contributors © CARTO", width="100%")
 
     draw = plugins.Draw(
         draw_options={
